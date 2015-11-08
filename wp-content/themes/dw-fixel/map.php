311831c24b3b716e8c51f907f132d3c6f161068d
Atanas Penchev <atanas.penchev@outlook.com>
2015-11-08 06:06:18 +0000

    Tidier code.

--- wp-content/themes/dw-fixel/map.php
+++ wp-content/themes/dw-fixel/map.php
@@ -9,146 +9,106 @@
 </div>
 <div id="main" role="main">
   <div class="single-page row-fluid">
-      <div class="main-content span12">
-        <div id="map" style="height: 600px; width: 100%"></div>
-      </div>
+    <div class="main-content span12">
+      <div id="map" style="height: 600px; width: 100%"></div>
+    </div>
   </div>
 </div>
 <?php get_footer() ?>
 
-    <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyC1Fuqax5sLGDq4KRkjaTobvhq-y-UyNVA"></script>
-    <script>
-        var imageSup = '<?= get_template_directory_uri(); ?>/img/iconSup3.png';
-        var imagePat = '<?= get_template_directory_uri(); ?>/img/iconPat4.png';
-        
-        var patients = [];
-        var supps= [];
-        
-        function onCooRecieved2(){
-        
-        network = [];
-        
-        for(var i = 0; i<patients.length; i++){
-            network.push({patient: patients[i].code, support: [] });
-            
-            for(var j = 0; j<supps.length; j++){
-                if(patients[i].name == supps[j].pat){
-                    network[i].support.push(supps[j].code);
-                }
-            }
-        }
-        
-            
-            var Glasgow = {lat: 55.852749, lng: -4.2209248};
+<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyC1Fuqax5sLGDq4KRkjaTobvhq-y-UyNVA"></script>
+<script>
+  var imageSup = '<?= get_template_directory_uri(); ?>/img/iconSup3.png';
+  var imagePat = '<?= get_template_directory_uri(); ?>/img/iconPat4.png';
     
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: Glasgow,
-                zoom: 12
-            });
+  var patients = [];
+  var supps= [];
+    
+  function onCooRecieved2() {
+    var network = [];
+    
+    for (var i = 0; i < patients.length; i++) {
+      network.push({patient: patients[i].code, support: []});
         
-        
-        
-            for(var i = 0; i<network.length; i++){
-            
-                var source = network[i].patient;
-            
-                var marker = new google.maps.Marker({
-                    position: source,
-                    map: map,
-                    mapTypeId: google.maps.MapTypeId.TERRAIN,
-                    icon: imagePat
-                });
-            
-            
-                var supporters = network[i].support;
-                
-                for(var j = 0; j<supporters.length; j++){
-                
-                    
-                
-                    var marker = new google.maps.Marker({
-                        position: supporters[j],
-                        map: map,
-                        mapTypeId: google.maps.MapTypeId.TERRAIN,
-                        icon: imageSup
-                    });
-                    
-                   
-                    
-                    var paths = [supporters[j], source];
-                
-                    var flightPath = new google.maps.Polyline({
-                        path: paths,
-                        geodesic: true,
-                        strokeColor: '#855081',
-                        strokeOpacity: 1.0,
-                        strokeWeight: 2
-                    });
-
-                    flightPath.setMap(map);
-                    
-                }
-            }      
+      for (var j = 0; j < supps.length; j++) {
+        if (patients[i].name == supps[j].pat) {
+          network[i].support.push(supps[j].code);
         }
-        
-        
-        function onCooRecieved(){
-        
-            var m = supps.length;
-            for(var i = 0; i<supps.length; i++){
-
-                supps[i] = {name: supps[i].title, code: supps[i].meta.post_code, pat: supps[i].meta.member.post_title};
-                
-                var apiUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + supps[i].code + "&sensor=false";
-                jQuery.getJSON( apiUrl, function( i, data ) {
-                    m--;
-                    supps[i].code = data.results[0].geometry.location;
-                    if(m == 0){
-                        onCooRecieved2();
-                    }
-                }.bind(this, i));
-                
-            }                                     
-        }
-        
-        
-        function onDataRecieved(){
-            
-            var l = patients.length;
-            
-            for(var i = 0; i<patients.length; i++){
-            
-                patients[i] = {name: patients[i].title, code: patients[i].meta.post_code};
-                
-                var apiUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + patients[i].code + "&sensor=false";
-                jQuery.getJSON( apiUrl, function( i, data ) {
-                    l--;
-                    patients[i].code = data.results[0].geometry.location;
-                    if(l == 0){
-                        onCooRecieved();
-                    }
-                }.bind(this, i));
-                
-                       
-            }     
-        }
-        
-        
-     
-     
-            
-     jQuery.getJSON( "http://5c1223b5.ngrok.com/cancer/team-6/wp-json/posts?type=member", function( data ) {
-        patients = data;
-            
-        jQuery.getJSON( "http://5c1223b5.ngrok.com/cancer/team-6/wp-json/posts?type=supporter", function( data ) {
-            supps = data;
-                
-            onDataRecieved();
-                
-        });
-            
+      }
+    }
+    
+    var Glasgow = {lat: 55.852749, lng: -4.2209248};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: Glasgow,
+      zoom: 12
     });
-    
-       
 
-    </script>
+    for (var i = 0; i < network.length; i++) {
+      var source = network[i].patient;
+    
+      var marker = new google.maps.Marker({
+        position: source,
+        map: map,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        icon: imagePat
+      });
+      
+      var supporters = network[i].support;
+      for (var j = 0; j < supporters.length; j++) {
+        var marker = new google.maps.Marker({
+          position: supporters[j],
+          map: map,
+          mapTypeId: google.maps.MapTypeId.TERRAIN,
+          icon: imageSup
+        });
+
+        var paths = [supporters[j], source];
+        var flightPath = new google.maps.Polyline({
+          path: paths,
+          geodesic: true,
+          strokeColor: '#855081',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+        
+        flightPath.setMap(map);
+      }
+    }      
+  }
+    
+  function onCooRecieved(){
+    var m = supps.length;
+    for (var i = 0; i < supps.length; i++) {
+      supps[i] = {name: supps[i].title, code: supps[i].meta.post_code, pat: supps[i].meta.member.post_title};
+      var apiUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + supps[i].code + "&sensor=false";
+      jQuery.getJSON( apiUrl, function( i, data ) {
+        m--;
+        supps[i].code = data.results[0].geometry.location;
+        if (m == 0) onCooRecieved2();
+      }.bind(this, i));
+            
+    }                                     
+  }
+    
+  function onDataRecieved() {
+    var l = patients.length;
+    for (var i = 0; i < patients.length; i++) {
+      patients[i] = {name: patients[i].title, code: patients[i].meta.post_code};
+      var apiUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + patients[i].code + "&sensor=false";
+      jQuery.getJSON( apiUrl, function( i, data ) {
+        l--;
+        patients[i].code = data.results[0].geometry.location;
+        if (l == 0) onCooRecieved();
+      }.bind(this, i));
+    }     
+  }
+
+  jQuery.getJSON( "http://5c1223b5.ngrok.com/cancer/team-6/wp-json/posts?type=member", function(data) {
+    patients = data;
+    jQuery.getJSON( "http://5c1223b5.ngrok.com/cancer/team-6/wp-json/posts?type=supporter", function(data) {
+      supps = data;
+      onDataRecieved();
+    });
+  });
+
+</script>
